b947749b9f9ef437433586977bf396c9de24251d
Mauricio Davico <mdavico@fi.uba.ar>
2023-11-11 23:37:38 -0300

    chore: connect test with racket repl

--- test/interprete_de_racket_en_clojure_davico_mauricio_106171/core_test.clj
+++ test/interprete_de_racket_en_clojure_davico_mauricio_106171/core_test.clj
@@ -1,7 +1,9 @@
 (ns interprete-de-racket-en-clojure-davico-mauricio-106171.core-test
   (:require [clojure.test :refer :all]
-            [interprete-de-racket-en-clojure-davico-mauricio-106171.core :refer :all]))
+            [interprete-de-racket-en-clojure-davico-mauricio-106171.racket]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 1))
+  )
+)
