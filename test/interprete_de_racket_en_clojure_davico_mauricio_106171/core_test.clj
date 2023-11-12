e1c25cd4c2934ac6d8f9fe7ce2742209954d45df
Mauricio Davico <mdavico@fi.uba.ar>
2023-11-12 00:45:27 -0300

    chore: erase dumb test

--- test/interprete_de_racket_en_clojure_davico_mauricio_106171/core_test.clj
+++ test/interprete_de_racket_en_clojure_davico_mauricio_106171/core_test.clj
@@ -2,12 +2,6 @@
   (:require [clojure.test :refer :all]
             [interprete-de-racket-en-clojure-davico-mauricio-106171.racket :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))
-  )
-)
-
 (deftest test-proteger-bool-en-str
   (testing "Proteger bool en str: Cambia, en una cadena, #t por %t y #f por %f"
     (is (= (proteger-bool-en-str "(or #f #t)") "(or %f %t)"))
